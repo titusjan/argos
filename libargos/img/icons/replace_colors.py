adfa9473030363e2a753adab541e0849b2e83f31
Pepijn Kenter <titusjan@gmail.com>
2014-12-21 13:34:23 +0100

    Added script to create different colored svg icons.

---
+++ libargos/img/icons/replace_colors.py
@@ -0,0 +1,34 @@
+#!/opt/local/bin/python
+# -*- coding: utf-8 -*-
+
+""" Reads SVG files and replaces the colors to produce different colored iconds
+"""
+import sys, os
+import fileinput
+
+
+
+def main():
+    snipIconBlue0 = '#008BFF'
+    snipIconBlue1 = '#00AAFF'
+    
+    newColors = ['#FF0000', '#00FF00']
+        
+    fileOut = None
+    for newColor in newColors:
+        
+        for fileNameIn in sys.argv[1:]:
+            with open(fileNameIn) as fileInput:
+                fileIn, extension = os.path.splitext(os.path.basename(fileNameIn))
+                fileNameOut = '{}.{}{}'.format(fileIn, newColor[1:], extension)
+                sys.stderr.write("Opening {}\n".format(fileNameOut))
+                with open(fileNameOut, 'w') as fileOut:
+                    for lineIn in fileInput:
+                        lineOut = lineIn.replace(snipIconBlue0, newColor).replace(snipIconBlue1, newColor)
+                        fileOut.write(lineOut)
+                        #print(lineOut)
+
+
+if __name__ == "__main__":
+    main()
+    
