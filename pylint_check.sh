54acb59082bec9b2a63fc8e6e4048f16191a5f05
Pepijn Kenter <titusjan@gmail.com>
2015-01-03 12:31:07 +0100

    Disabled some pylint checks and files

--- pylint_check.sh
+++ pylint_check.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 export PYTHONPATH=$PYTHONPATH:./tools:./ocal/lib/examples
-find . -type f \( -iname "*.py" ! -iname "__init__.py" ! -iname "conf.py"  \) | xargs pylint --rcfile=.pylintrc '--msg-template={path}:{line}: [{msg_id}({symbol}), {obj}] {msg}' -f parseable $* | tee pylint.out
+find . -type f \( -iname "*.py" ! -iname "__init__.py" ! -iname "setup.py" ! -iname "conf.py" ! -wholename "./tests/*" \) | xargs pylint-3.4 --rcfile=.pylintrc '--msg-template={path}:{line}: [{msg_id} ({symbol}), {obj}] {msg}' $* | tee pylint.out
 
