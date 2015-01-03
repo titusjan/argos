847e6a5d371a02f7f73e59116b80b122e434ece7
Pepijn Kenter <titusjan@gmail.com>
2015-01-03 13:52:47 +0100

    pylint_check.sh can work on specified source files.

--- pylint_check.sh
+++ pylint_check.sh
@@ -1,4 +1,13 @@
 #! /bin/bash
-export PYTHONPATH=$PYTHONPATH:./tools:./ocal/lib/examples
-find . -type f \( -iname "*.py" ! -iname "__init__.py" ! -iname "setup.py" ! -iname "conf.py" ! -wholename "./tests/*" \) | xargs pylint-3.4 --rcfile=.pylintrc '--msg-template={path}:{line}: [{msg_id} ({symbol}), {obj}] {msg}' $* | tee pylint.out
+
+# Performs pylint check on the given files. If no files are given it will search in the project for all relevant source files.
+
+if [[ -z "$*" ]] ; then
+    input_files=$(find . -type f \( -iname "*.py" ! -iname "__init__.py" ! -iname "setup.py" ! -iname "conf.py" ! -wholename "./tests/*" \) )
+else
+    input_files="$*"
+fi
+
+#echo ${input_files}
+pylint-3.4 --rcfile=.pylintrc '--msg-template={path}:{line}: [{msg_id} ({symbol}), {obj}] {msg}' ${input_files}
 
