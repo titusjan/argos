7986a34b8d946462d5ab3f93c3ee8329ff4a58b1
Pepijn Kenter <titusjan@gmail.com>
2015-02-14 13:49:19 +0100

    Activate window on darwin

--- libargos/application.py
+++ libargos/application.py
@@ -147,7 +147,12 @@ class ArgosApplication(object):
         
         mainWindow.show()
         if platform.system() == 'Darwin':
+            # Calling raise when before the QApplication.exec_ only shows the last window
+            # that was added. Therefore we also call activeWindow. However, this may not
+            # always be desirable. TODO: make optional? 
             mainWindow.raise_()
+            mainWindow.activateWindow() 
+            
         return mainWindow
     
     
