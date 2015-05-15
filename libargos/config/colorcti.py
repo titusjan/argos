f862e584e24bb6dc8de42205903a7fd194dcb1c1
Pepijn Kenter <titusjan@gmail.com>
2015-05-15 08:57:32 +0200

    Tooltip text

--- libargos/config/colorcti.py
+++ libargos/config/colorcti.py
@@ -93,7 +93,7 @@ class ColorCtiEditor(AbstractCtiEditor):
         
         pickButton = QtGui.QToolButton()
         pickButton.setText("...")
-        pickButton.setToolTip("Opens color dialog.")
+        pickButton.setToolTip("Open color dialog.")
         #pickButton.setIcon(QtGui.QIcon(os.path.join(icons_directory(), 'err.warning.svg')))
         pickButton.setFocusPolicy(Qt.NoFocus)
         pickButton.clicked.connect(self.openColorDialog)
@@ -119,7 +119,6 @@ class ColorCtiEditor(AbstractCtiEditor):
         qColor = QtGui.QColorDialog.getColor(currentColor, self)
         
         if qColor.isValid():
-            logger.debug("color: {} (valid={})".format(qColor, qColor.isValid()))
             self.setData(qColor)
             self.commitAndClose()
         
