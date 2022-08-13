9b68e827716f30f3ff98a468b72c430a93be5e89
Pepijn Kenter <titusjan@gmail.com>
2022-08-13 12:46:28 +0200

    New sphinx skeleton structure.

---
+++ docs/source/conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Argos'
+copyright = '2022, Pepijn Kenter'
+author = 'Pepijn Kenter'
+release = '0.4.3'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx_rtd_theme',     # Read-the-docs theme
+    'sphinx.ext.napoleon',  # Support for Google style docstrings
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+add_module_names = False
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
